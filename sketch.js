d6e0f7b872bf2a132a7bb8b4297978cdfd8280ba
94515kevin <150640275+94515kevin@users.noreply.github.com>
2024-04-29 11:47:08 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,10 +14,14 @@ function setup() {
   capture.hide()
   //----------------------選鈕
   radioElement = createRadio();
-  radioElement.position(width-2-300,20)
+  radioElement.position(width-2-600,20)
   radioElement.option("方塊")
   radioElement.option("圓圈")
+  radioElement.option("亮度")
+  radioElement.option("紅底")
+  radioElement.option("文字")
   radioElement.style("color","#fff")
+  radioElement.style("font-size","30px")
 }
 
 function draw() {
@@ -31,11 +35,36 @@ function draw() {
     for(var y=0;y<captureGraphics.height ; y=y+span){
       var pixel = captureGraphics.get(x,y)
       fill(pixel)
-      if(radioElement.value()=="方塊"){
+      if(radioElement.value()=="方塊" || radioElement.value()==""){
         rect(x,y,span)
       }
       if(radioElement.value()=="圓圈"){
         ellipse(x,y,span)
+      }
+      if(radioElement.value()=="亮度"){
+        bk = (pixel[0]+pixel[1]+pixel[2])/3
+        fill(bk)
+        ellipse(x,y,span)
+        ellipse(x,y,span*map(bk,0,255,0,1))//透過map指令，bk在0~255間，依照比率設定0~1
+      }
+      if(radioElement.value()=="紅底"){
+        colorMode(HSB)
+        fill(pixel[0],80,80)
+        push()
+        translate(x,y)
+        rectMode(CENTER)
+        rect(x,y,span*0.8)
+        fill(0)
+        ellipse(0,0,10)//透過map指令，bk在0~255間，依照比率設定0~1
+      }
+      if(radioElement.value()=="文字"){
+        const density = 'Ñ@#W$9876543210?!abc;:+=-,._ ';
+        let txt = "一二三四五田雷電龕龘"
+        bk = (pixel[0]+pixel[1]+pixel[2])/3
+        let bkId = int(map(bk, 0, 255, 9, 0))
+        text(txt[bkId])
+        textStyle(BOLD)
+        textSize(span)
       }
     }
   }
