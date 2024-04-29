c22851f836a5fbc33a88a2b09f87fab246d73d3c
94515kevin <150640275+94515kevin@users.noreply.github.com>
2024-04-29 10:28:48 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,30 @@
+var captureGraphics
+var capture_width = 640
+var capture_height = 480
+var span = 5
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   capture = createCapture(VIDEO)
-  capture.size(320,240);
+  capture.size(capture_width,capture_height);
+  captureGraphics = createGraphics(capture_width,capture_height)
+  captureGraphics.translate(capture_width,0)
+  captureGraphics.scale(-1,1)
+  captureGraphics.hide()
 }
 
 function draw() {
   background(220);
+  noStroke()
   push()
-  translate(width/2-160,height/2-120)
-  image(capture,0, 0)
+  translate(width/2-capture_width/2,height/2-capture_height/2)
+  captureGraphics.image(capture,0, 0)
+  for(var x = 0;x<captureGraphics.width;x=x+10){
+    for(var y=0;y<captureGraphics.height ; y=y+10){
+      var pixel = captureGraphics.get(x,y)
+      fill(pixel)
+      rect(x,y,span)
+    }
+  }
   pop()
 }
